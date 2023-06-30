7a7d4378753d9e1c29cbdcbc7cb31f823692966d
thyeone <thyeonee@gmail.com>
2023-06-30 16:48:51 +0900

    solve: 봉우리

--- 02-Array/봉우리.js
+++ 02-Array/봉우리.js
@@ -1,29 +1,22 @@
 function solution(arr) {
   let answer = 0;
-  let n = arr.length;
-  let dx = [-1, 0, 1, 0];
-  let dy = [0, 1, 0, -1];
+  const n = arr.length;
+  const dx = [-1, 0, 1, 0];
+  const dy = [0, 1, 0, -1];
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
       let flag = 1;
       for (let k = 0; k < 4; k++) {
         let nx = i + dx[k];
         let ny = j + dy[k];
-        if (
-          nx >= 0 &&
-          nx < n &&
-          ny >= 0 &&
-          ny < n &&
-          arr[nx][ny] >= arr[i][j]
-        ) {
+        if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] >= arr[i][j]) {
           flag = 0;
           break;
         }
       }
-      if (flag) answer++;
+      if (flag === 1) answer++;
     }
   }
-
   return answer;
 }
 
