5a4f4650e6649231afd1a44487b4e4baca57b3a3
thyeone <thyeonee@gmail.com>
2023-07-31 18:01:19 +0900

    solve: 연속부분수열1

---
+++ 05-TwoPointer/연속부분수열1.js
@@ -0,0 +1,17 @@
+function solution(M, arr) {
+  let answer = 0;
+  let lt = 0;
+  let sum = 0;
+  for (let rt = 0; rt < arr.length; rt++) {
+    sum += arr[rt];
+    if (sum === M) answer++;
+    while (sum >= M) {
+      sum -= arr[lt++];
+      if (sum === M) answer++;
+    }
+  }
+  return answer;
+}
+
+let a = [1, 2, 1, 3, 1, 1, 1, 2];
+console.log(solution(6, a));
