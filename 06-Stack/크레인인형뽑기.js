0529821c9847dfb0d5b91770f55fdfc94c6ddebb
thyeone <thyeonee@gmail.com>
2023-09-09 23:31:03 +0900

    solve: 크레인인형뽑기

---
+++ 06-Stack/크레인인형뽑기.js
@@ -0,0 +1,30 @@
+function solution(board, moves) {
+  let stack = [];
+  let answer = 0;
+
+  for (let i = 0; i < moves.length; i++) {
+    for (let j = 0; j < board.length; j++) {
+      if (board[j][moves[i] - 1] !== 0) {
+        stack.push(board[j][moves[i] - 1]);
+        board[j][moves[i] - 1] = 0;
+        break;
+      } else if (stack[stack.length - 1] === stack[stack.length - 2]) {
+        stack.pop() && answer++;
+        stack.pop() && answer++;
+      }
+    }
+  }
+
+  return answer;
+}
+
+let a = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 3],
+  [0, 2, 5, 0, 1],
+  [4, 2, 4, 4, 2],
+  [3, 5, 1, 3, 1],
+];
+
+let b = [1, 5, 3, 5, 1, 2, 1, 4];
+console.log(solution(a, b));
