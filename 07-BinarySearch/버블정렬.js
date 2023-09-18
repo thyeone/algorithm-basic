721ca8bbe9270ac198384d0c6d83d596a935a8bb
thyeone <thyeonee@gmail.com>
2023-09-18 18:36:11 +0900

    solve: 버블정렬

--- 07-BinarySearch/버블정렬.js
+++ 07-BinarySearch/버블정렬.js
@@ -2,9 +2,9 @@ function solution(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = i; j < arr.length - 1; j++) {
       if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      console.log(arr[j], arr[j + 1]);
     }
   }
-  console.log(arr);
 }
 
 let arr = [13, 5, 11, 7, 23, 15];
