90f0d6ed81e135ec2774210b90b0d45160a3f23d
thyeone <thyeonee@gmail.com>
2023-09-20 19:48:35 +0900

    solve: 삽입정렬

--- 07-BinarySearch/삽입정렬.js
+++ 07-BinarySearch/삽입정렬.js
@@ -3,7 +3,6 @@ function solution(arr) {
   for (let i = 0; i < arr.length; i++) {
     let currentElement = arr[i];
     let j = i - 1;
-
     while (j >= 0 && arr[j] > currentElement) {
       arr[j + 1] = arr[j];
       j--;
