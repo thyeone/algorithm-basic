1a9c4dfeb9a0d57d1be6e76a410d31bf1b6e38f7
thyeone <thyeonee@gmail.com>
2023-10-14 17:24:57 +0900

    solve: 최대점수구하기

---
+++ 08-DFS/최대점수구하기.js
@@ -0,0 +1,19 @@
+function solution(M, ps, pt) {
+  let answer = Number.MIN_SAFE_INTEGER;
+
+  function DFS(L, sum, time) {
+    if (time > M) return;
+
+    if (L === ps.length) answer = Math.max(answer, sum);
+    else {
+      DFS(L + 1, sum + ps[L], time + pt[L]);
+      DFS(L + 1, sum, time);
+    }
+  }
+
+  DFS(0, 0, 0);
+}
+
+let ps = [10, 25, 15, 6, 7];
+let pt = [5, 12, 8, 3, 4];
+console.log(solution(20, ps, pt));
